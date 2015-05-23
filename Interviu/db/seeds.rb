903dcd4299f093e400d8849681731dc07c062d28
Skillvendor <Skillvendor193@gmail.com>
2015-05-23 15:11:57 +0300

    For testing, god it takes long to seed 11000 records

--- Interviu/db/seeds.rb
+++ Interviu/db/seeds.rb
@@ -11,3 +11,11 @@ Product.create(title: "Pahare", description: "Cele mai tari pahare!")
 Product.create(title: "Servetele", description: "De masa")
 Product.create(title: "Laptopuri", description: "Alienware")
 Product.create(title: "Mac", description: "nu cumpara...!")
+
+1000.times do |g|
+	Product.create(title: Faker::Commerce.product_name, description: "Who Cares..")
+end
+
+10000.times do |c|
+	Variant.create(price: (rand(100)).floor, quantity: (rand(100)).floor , product_id: rand(1..600).floor)
+end
