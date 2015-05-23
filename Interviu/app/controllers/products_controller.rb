7ddb97ddf2993e83b922046b33a4f173439a0dab
Skillvendor <Skillvendor193@gmail.com>
2015-05-23 12:12:19 +0300

    Must be logged in before viewing products

--- Interviu/app/controllers/products_controller.rb
+++ Interviu/app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_buyer!
 
   # GET /products
   # GET /products.json
